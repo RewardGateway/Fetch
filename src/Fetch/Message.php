fcd06b6d746e1ec70cd03b1b01da9b104ce531aa
Miguel Guerreiro <miguel.guerreiro@rewardgateway.com>
2015-03-05 13:44:38 +0000

    use lowercase on parsed raw headers' indexes
    Signed-off-by: Miguel Guerreiro <miguel.guerreiro@rewardgateway.com>

--- src/Fetch/Message.php
+++ src/Fetch/Message.php
@@ -362,7 +362,9 @@ class Message
                     $this->rawParsed[$lastHeader] .= "\n" . trim($header);
                 } else {
                     list($lastHeader, $value) = explode(':', $header, 2);
+                    $lastHeader = strtolower($lastHeader);
 
+                    // append to existing headers instead of overwriting
                     if (isset($this->rawParsed[$lastHeader])) {
                         $this->rawParsed[$lastHeader] .= "\n" . trim($value);
                     } else {
@@ -371,6 +373,9 @@ class Message
                 }
             }
         }
+
+        // use only lowercase indexes
+        $name = strtolower($name);
 
         if (isset($this->rawParsed[$name])) {
             return $this->rawParsed[$name];
