2f51f7cf7f251451c603348a18213f5e04995cd8
Miguel Guerreiro <miguel.guerreiro@rewardgateway.com>
2015-03-05 12:01:11 +0000

    Error when address was 'undisclosed-recipients'
    Signed-off-by: Miguel Guerreiro <miguel.guerreiro@rewardgateway.com>

--- src/Fetch/Message.php
+++ src/Fetch/Message.php
@@ -702,12 +702,15 @@ class Message
 
         if (is_array($addresses)) {
             foreach ($addresses as $address) {
-                if (property_exists($address, 'mailbox') && $address->mailbox != 'undisclosed-recipients') {
-                    $currentAddress = array();
-                    $currentAddress['address'] = $address->mailbox . '@' . $address->host;
+                if (property_exists($address, 'mailbox')) {
+                    if ($address->mailbox != 'undisclosed-recipients') {
+                        $currentAddress['address'] = $address->mailbox . '@' . $address->host;
 
-                    if (isset($address->personal)) {
-                        $currentAddress['name'] = MIME::decode($address->personal, self::$charset);
+                        if (isset($address->personal)) {
+                            $currentAddress['name'] = MIME::decode($address->personal, self::$charset);
+                        }
+                    } else {
+                        $currentAddress['address'] = $address->mailbox;
                     }
 
                     $outputAddresses[] = $currentAddress;
